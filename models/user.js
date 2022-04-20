e4b2800edf1a02a5c1b4d86397030c3f82f8373e
Romain Rey <romainrey.contact@gmail.com>
2022-04-20 14:31:31 +0200

    feat: Ajout d'un schéma d'user

---
+++ models/user.js
@@ -0,0 +1,32 @@
+//création d'un model d'user* avec mongoose
+const mongoose = require('mongoose');
+require('mongoose-type-email');
+
+//vérification avec des plugins de validation
+const mongooseUniqueValidator = require('mongoose-unique-validator');
+const mongooseSanitizerPlugin = require('mongoose-sanitizer-plugin');
+
+//création du schéma de données pour user*
+const userSchema = mongoose.Schema({
+    //l'adresse email unique
+    email: {
+        type: String,
+        unique: true,
+        required: [true, "Veuillez entrer votre email"],
+        match: [/^[a-z0-9.]+@{1,}[a-z0-9]{2,}\.[a-z]{2,4}$/gi, "Veuillez entrer un email valide"]
+    },
+    //l'enregistrement de password par l'utilisateur
+    password: {
+        type: String,
+        required: [true, "Veuillez entrer un mot de passe"]
+    }
+});
+
+//plugin pour garantir une adresse email unique
+userSchema.plugin(mongooseUniqueValidator);
+
+//plugin pour purifier les champs du model afin de les enregistrer après dans MongoDB
+userSchema.plugin(mongooseSanitizerPlugin);
+
+//exportation de userSchema 
+module.exports = mongoose.model(userSchema);
