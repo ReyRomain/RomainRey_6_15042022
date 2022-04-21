eb699a1d5aae6305866195ed42aa49965ae45d5a
Romain Rey <romainrey.contact@gmail.com>
2022-04-21 11:49:18 +0200

    feat: Mise en place de jsonWebToken

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+//récupération de jsonwebtoken
+const jsonwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.Authorization.split(' ')[1];
+        const decodedToken = jsonwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = decodedToken.userId;
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'Invalid user ID';
+        } else {
+            next();
+        }
+    } catch {
+        res.status(401).json({
+            error: new Error('Invalid request!'),
+        });
+    }
+};
