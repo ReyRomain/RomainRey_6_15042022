a90fa11327ac56ace5d31a03858fc4181e68f000
Romain Rey <romainrey.contact@gmail.com>
2022-04-25 22:54:47 +0200

    feat: Ajout de la fonction login

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -10,6 +10,31 @@ const jsonwt = require('jsonwebtoken');
 
 function login(req,res, next){
 
+    //on récupère l'user de la base de donnée qui correspond à l'adresse mail entrée
+    User.findOne({ email: req.body.email })
+
+    //si on ne reçoit pas de user, on envoie une erreur
+    .then(user => {
+        if (!user) { 
+            return res.status(401).json({ error: 'User not found !' });
+        }
+
+        //on compare le password entré avec le hash qui est gardé dans la base de donnée
+        bcrypt.compare(req.body.password, user.password)
+
+            //si la comparaison n'est pas bonne, on envoie une erreur
+            .then(valid => {
+                if (!valid) { return res.status(401).json({ error: 'Invalid password !' });
+                }
+                //sinon on renvoie son userId et un token
+                res.status(200).json({
+                    userId: user._id,
+                    token: 'TOKEN'
+                });
+            })
+            .catch(error => res.status(500).json({ error }));
+        })
+    .catch(error => res.status(500).json({ error }));
 }
 
 async function signup(req,res, next){
