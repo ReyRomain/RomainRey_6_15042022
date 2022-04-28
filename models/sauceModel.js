a29ee13813c8258d5faddb23c4da74efa2e2e9f9
Romain Rey <romainrey.contact@gmail.com>
2022-04-28 21:02:51 +0200

    feat: Début de sauceSchema

--- models/sauceModel.js
+++ models/sauceModel.js
@@ -9,6 +9,83 @@ const mongooseSanitizerPlugin = require('mongoose-sanitizer-plugin');
  */
  const sauceSchema = mongoose.Schema({
 
+    /**
+     * l'identifiant MongoDB unique de l'utilisateur qui a créé la sauce
+     */
+    userId: {
+        type: String,
+        require: true
+    },
+
+    /**
+     * nom de la sauce
+     */
+    name: {
+
+    },
+
+    /**
+     * fabricant de la sauce
+     */
+    manufacturer: {
+
+    },
+
+    /**
+     * description de la sauce
+     */
+    description: {
+
+    },
+
+    /**
+     * le principal ingrédient épicé de la sauce
+     */
+    mainPepper: {
+
+    },
+
+    /**
+     * l'URL de l'image de la sauce téléchargée par l'utilisateur
+     */
+    imageUrl: {
+
+    },
+
+    /**
+     * nombre entre 1 et 10 décrivant la sauce
+     */
+    heat: {
+
+    },
+
+    /**
+     * nombre d'utilisateurs qui aiment la sauce
+     */
+    likes: {
+
+    },
+
+    /**
+     * nombre d'utilisateurs qui n'aiment pas la sauce
+     */
+    dislikes: {
+
+    },
+
+    /**
+     * tableau des identifiants des utilisateurs qui ont aimé la sauce
+     */
+    usersLiked: {
+
+    }
+
+    /**
+     * tableau des identifiants des utilisateurs qui n'ont pas aimé la sauce
+     */
+    usersDisliked: {
+
+    }
 });
 
  /**
