856e5d0a6b514cbd06db7138b22385ce1b54674c
Romain Rey <romainrey.contact@gmail.com>
2022-04-28 19:30:12 +0200

    feat: Mise en place de la route pour les sauces

---
+++ routes/sauceRoute.js
@@ -0,0 +1,7 @@
+//ce fichier contient les différentes fonctions de routes pour les sauces*
+
+//utilisation de Express
+const express = require('express')
+
+//on utlise le router mis à disposition par Express
+const router = express.Router();
