13d9aa4450e2069dd3ec9a131c19d31271baed75
Romain Rey <romainrey.contact@gmail.com>
2022-04-28 22:14:54 +0200

    feat: Mise en place des routes pour les sauces

--- routes/sauceRoute.js
+++ routes/sauceRoute.js
@@ -1,7 +1,34 @@
 //ce fichier contient les différentes fonctions de routes pour les sauces*
 
-//utilisation de Express
+/**
+ * utilisation de Express
+ */
 const express = require('express')
 
-//on utlise le router mis à disposition par Express
+/**
+ * importation du middleware auth pour sécuriser les routes
+ */
+const auth = require("../middlewares/auth");
+
+/**
+ * importation du middleware multer pour la gestion des images
+ */
+const multer = require("../middlewares/multer-config");
+
+/**
+ * on utilise le router mis à disposition par Express
+ */
 const router = express.Router();
+
+
+
+
+
+
+
+
+
+/**
+ * exportation de router
+ */
+module.exports = router;
