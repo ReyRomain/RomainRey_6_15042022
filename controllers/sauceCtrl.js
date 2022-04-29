0da0faa54e870076648fe3724b7ff09b4c8ec03d
Romain Rey <romainrey.contact@gmail.com>
2022-04-29 09:57:20 +0200

    fix: ecriture es8 de getAllSauces

--- controllers/sauceCtrl.js
+++ controllers/sauceCtrl.js
@@ -3,16 +3,16 @@
  */
 const Sauce = require('../models/sauceModel.js');
 
-function getAllSauces (req, res, next) {
+async function getAllSauces (req, res, next) {
 
     //on utilise find pour récupérer le tableau des sauces dans la base de données
-    Sauce.find()
-
-    //si true on retourne le tableau
-    .then(sauces => res.status(200).json(sauces))
-
-    //sinon message d'erreur
-    .catch(error => res.status(400).json({ error }));
+    try {
+        const sauces = await Sauce.find();
+        res.status(200).json(sauces);
+    } catch (error) {
+        console.warn(error)
+        res.status(400).json({ error })
+    }
 }
 
 function getSauce (req, res, next) {
