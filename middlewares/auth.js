b660403a4375dce2eb7a4c369492a0e9c3e20ef9
Romain Rey <romainrey.contact@gmail.com>
2022-05-14 09:58:31 +0200

    fix: Correction du middleware auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,16 +5,15 @@ const jsonwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        if (! req.body.userId) throw 'Missing authentification';
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jsonwt.verify(token, process.env.JWT_PASS);
         // @ts-ignore
-        if (req.body.userId !== decodedToken.userId) throw 'Invalid user ID';
+        if (req.body.userId && req.body.userId !== decodedToken.userId) throw 'Invalid user ID';
         next();
     } catch (error) {
         console.log(error);
         res.status(401).json({
-            error,
+            error
         });
     }
 };
