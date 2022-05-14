a95dcaaa305943c87dd691dc01dd495b5946b600
Romain Rey <romainrey.contact@gmail.com>
2022-05-14 19:10:16 +0200

    docs: Ajout de commentaire

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,17 @@
 /**
- * récupération de jsonwebtoken
+ * Récupération de jsonwebtoken
  */
 const jsonwt = require('jsonwebtoken');
 
+/**
+ * Middleware d'authentification
+ *
+ * @param   {Object}    req   [req description]
+ * @param   {Object}    res   [res description]
+ * @param   {Function}  next  [next description]
+ *
+ * @return  {void}
+ */
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
